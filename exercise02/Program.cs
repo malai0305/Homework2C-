d75ebacd4a59b230578c960e972a9dee33fa299e
unknown <artem03051987@gmail.com>
2022-08-14 13:10:02 +0500

    квадрат числа

---
+++ exercise02/Program.cs
@@ -0,0 +1,12 @@
+﻿// По двум заданным числам проверить является ли одно квадратом другого.
+int a = 4;
+int b = 3;
+if ( a*a == b)
+{
+    Console.WriteLine("YES");
+}
+else if( a*a < b ) if ( a*a > b )
+{
+    Console.WriteLine("NO");
+}
+    
